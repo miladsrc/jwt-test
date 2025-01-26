6e067808d4a6584b9dbc2bec39c75ff650051ff7
milad <baranimiald14@gmail.com>
2025-01-26 09:46:14 +0330

    feat: implement user detail

---
+++ src/main/java/com/jwttest/user/Role.java
@@ -0,0 +1,7 @@
+package com.jwttest.user;
+
+public enum Role {
+
+    USER, ADMIN
+
+}
--- src/main/java/com/jwttest/user/User.java
+++ src/main/java/com/jwttest/user/User.java
@@ -1,11 +1,14 @@
 package com.jwttest.user;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Getter
@@ -17,7 +20,7 @@ import lombok.*;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
 
     @Id
     private Long id;
@@ -40,4 +43,35 @@ public class User {
     @Column(name = "PHONE", nullable = false)
     private String phone;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+
+    //USER DETAILS METHODS
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
